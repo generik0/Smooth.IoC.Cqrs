58b565957a69d91e1c34f0f5f44e7a8ce963a58e
Rik Svendsen Rose <rsr@gait.dk>
2016-12-31 11:07:37 +0100

    Add Query handler with no query model

---
+++ src/Smooth.IoC.Cqrs/Query/IQueryHandler'2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Smooth.IoC.Cqrs.Query
+{
+    public interface IQueryHandler<TResult> : IDisposable 
+        where TResult : class
+    {
+        /// <summary>
+        /// Execute a query asynchronously.
+        /// </summary>
+        /// <returns>Task which will be completed once the query has been executed.</returns>
+        Task<IEnumerable<TResult>> QueryAsync();
+
+        /// <summary>
+        /// Execute a query synchronously.
+        /// </summary>
+        /// <returns>Task which will be completed once the query has been executed.</returns>
+        IEnumerable<TResult> Query();
+    }
+}
