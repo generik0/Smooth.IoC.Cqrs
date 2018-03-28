6d6239bb32f69f1f60a86cfb96d5e91d66155dc9
Rik Svendsen Rose <rsr@gait.dk>
2018-03-28 20:47:25 +0200

    Add test adapter for VS build

--- build/Build.Locally.ps1
+++ build/Build.Locally.ps1
@@ -2,7 +2,7 @@
 
 $packageVersion = "0.0.1-C1"
 $configuration = "Release"
-$msBuildExe = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+$msBuildExe = "C:\Program Files (x86)\MSBuild\15.0\Bin\MSBuild.exe"
 $msBuildTarget = "ReBuild"
 
 $CommandDirectory = split-path -parent $MyInvocation.InvocationName
